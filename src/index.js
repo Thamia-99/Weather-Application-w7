9d13add7b38bee3274ec881f7f97a16e88001d53
Thamia-99 <78696837+Thamia-99@users.noreply.github.com>
2024-02-23 11:38:34 +0200

    Adjusted weather icon and integrated Api data

--- src/index.js
+++ src/index.js
@@ -67,27 +67,41 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function displayForecast(response) {
   console.log(response.data);
 
   let forecastElement = document.querySelector("#forecast");
 
-  let days = ["Tues", "Wed", "Thur", "Fri", "Sat"];
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 6) {
+      forecastHtml =
+        forecastHtml +
+        `
   <div class="weather-forecast-day">
-    <div class="weather-forecast-date">${day}</div>
-    <div class="weather-forecast-icon">🌤️</div>
+    <div class="weather-forecast-date">${formatDay(day.time)}</div>
+    
+    <img src="${day.condition.icon_url}" class="weather-forecast-icon"/>
+    
     <div class="weather-forecast-temp">
-      <span class="temperature-max"> 18°</span>
-      <span class="temperature-min">12°</span>
+      <span class="temperature-max"> ${Math.round(
+        day.temperature.maximum
+      )}°</span>
+      <span class="temperature-min">${Math.round(
+        day.temperature.minimum
+      )}°</span>
     </div>
   </div>
 `;
+    }
   });
 
   forecastElement.innerHTML = forecastHtml;
